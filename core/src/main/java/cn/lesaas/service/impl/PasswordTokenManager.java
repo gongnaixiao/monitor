496e476be2e8e2e60f8320ed6f4d664901ad85c2
gongnaixiao <gongnaixiao@163.com>
2014-12-24 10:23:37 +0800

    initial project version

---
+++ core/src/main/java/cn/lesaas/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package cn.lesaas.service.impl;
+
+import cn.lesaas.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
